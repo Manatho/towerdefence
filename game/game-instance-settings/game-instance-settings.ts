e8ad95fc17a29b81352144f0ccaa443cfefaf727
Mathies <mathies-h@hotmail.com>
2021-12-18 10:15:09 +0100

    Tweaked map

--- game/game-instance-settings/game-instance-settings.ts
+++ game/game-instance-settings/game-instance-settings.ts
@@ -1,6 +1,6 @@
 import { GridMap } from "../map/grid-map";
 
-let defaultMapTextures = ["#44b376", "#f0ecad", "#30a6d9"];
+let defaultMapTextures = ["#44b376", "#f0ecad", "#30a6d9", "#798c94" ];
 
 export const GameInstanceSettings = {
   maps: [],
@@ -8,14 +8,14 @@ export const GameInstanceSettings = {
 };
 
 let map1 = new GridMap([
-  [0, 0, 0, 0, 0, 0, 0, 0], //
-  [1, 0, 0, 0, 0, 0, 0, 0],
-  [2, 1, 0, 0, 0, 0, 0, 0],
-  [2, 2, 1, 0, 0, 0, 0, 0],
-  [2, 2, 1, 0, 0, 0, 0, 0],
-  [2, 2, 1, 0, 0, 0, 0, 0],
-  [2, 2, 2, 1, 0, 0, 0, 0],
-  [2, 2, 2, 2, 1, 0, 0, 0],
+  [0, 0, 0, 3, 0, 0, 0, 0], //
+  [1, 0, 0, 3, 0, 0, 0, 0],
+  [2, 1, 0, 3, 3, 3, 0, 0],
+  [2, 2, 1, 0, 0, 3, 0, 0],
+  [2, 2, 1, 0, 0, 3, 0, 0],
+  [2, 2, 1, 0, 0, 3, 3, 0],
+  [2, 2, 2, 1, 0, 0, 3, 0],
+  [2, 2, 2, 2, 1, 0, 3, 0],
 ]);
 
 GameInstanceSettings.maps.push(map1);
